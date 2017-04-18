99bc4ec3c442ec2256471c6407f07039a9d10633
JF Gamache <jegamach@microsoft.com>
2017-04-18 12:42:30 -0400

    modify the custom script

--- vmsstemplate/CustomScripts/install_apache.sh
+++ vmsstemplate/CustomScripts/install_apache.sh
@@ -1,1 +1,16 @@
 # Custom Script for Linux
+
+#!/bin/bash
+
+apt-get -y update
+
+
+# install Apache2
+apt-get -y install apache2 
+
+
+# write some HTML
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+apachectl restart
